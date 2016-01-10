cdde1d3fc1f77abc36566997baa85f07aa5cb704
PeterDG <peterdavid_gutierrez@hotmail.com>
2016-01-10 13:30:38 -0500

    added class ZonedDate for treat the dates with time zone

---
+++ src/main/java/com/peter/util/ZonedDate.java
@@ -0,0 +1,39 @@
+package com.peter.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by Peter on 1/9/2016.
+ */
+public class ZonedDate {
+
+    ZonedDateTime date;
+    DateTimeFormatter formatter;
+
+    public ZonedDate(String date) {
+        this.date = ZonedDateTime.parse(date);
+        formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    }
+
+    public ZonedDateTime setZoneOffset(String strZone){
+        LocalDateTime dt = LocalDateTime.now();
+        ZoneId zone = ZoneId.of(strZone);
+        ZonedDateTime zdt = dt.atZone(zone);
+        ZoneOffset offset = zdt.getOffset();
+        date= date.withZoneSameInstant(offset);
+        return date;
+    }
+
+    public String toString(){
+        String formattedDate = formatter.format(date);
+        return formattedDate;
+    }
+
+    public void setFormatter(DateTimeFormatter formatter) {
+        this.formatter = formatter;
+    }
+}
---
+++ src/main/test/com/peter/util/DateTest.java
@@ -0,0 +1,17 @@
+package com.peter.util;
+
+import org.junit.Test;
+
+/**
+ * Created by Peter on 1/10/2016.
+ */
+public class DateTest {
+
+    @Test
+    public void testZoneOffset() throws Exception {
+        ZonedDate date = new ZonedDate("2014-09-14T17:00:00+00:00");
+        System.out.println(date.toString());
+        date.setZoneOffset("America/Bogota");
+        System.out.println(date.toString());
+    }
+}
