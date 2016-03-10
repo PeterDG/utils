20425b51844e084b1239af9418b212b056f39450
PeterDG <peterdavid_gutierrez@hotmail.com>
2016-03-09 21:08:43 -0500

    add new method in the JDBC class for insert data on the table

--- src/main/java/com/peter/util/db/DBManager.java
+++ src/main/java/com/peter/util/db/DBManager.java
@@ -21,6 +21,7 @@ public interface DBManager {
  boolean existDB(String dbName);
  void createTable(String table, String columnNamesAndTypes, Optional<String> primaryKey);
  void deleteTable(String dbName);
+ void insertTable(String testTable, ArrayList<DBTable> dbTables);
  void insertTable(String table, String columnNames, String values);
  void insertTable(String table,  ArrayList<String> columnNamesList, ArrayList<String> valuesList);
  void updateTable(String table, String columnNames, String values,  Optional<String> where);
@@ -29,4 +30,5 @@ public interface DBManager {
  boolean isQuerySuccessful();
  void closeConnection();
  void commit();
+
 }
